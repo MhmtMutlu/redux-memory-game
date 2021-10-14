af053a7041dc3b2b5af0bdbd8cd8a3029c844caf
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-10-14 10:41:11 +0300

    UI: Header component was created and designed.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeaderContainer, HeaderTitle } from "./styles";
+
+const Header: React.FC = () => {
+  return (
+    <HeaderContainer>
+      <HeaderTitle>Catch and Match</HeaderTitle>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HeaderTitle = styled.h1`
+  font-size: 40px;
+`;
