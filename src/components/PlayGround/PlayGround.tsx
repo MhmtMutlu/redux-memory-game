a8488801c3fa3872d2b8817aa7473ebfdc011085
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-10-15 21:41:46 +0300

    Feat-UI: Redux actions were used in PlayGround component and it is designed again.

--- src/components/PlayGround/PlayGround.tsx
+++ src/components/PlayGround/PlayGround.tsx
@@ -1,10 +1,38 @@
-import React from "react";
-import { GameBoard, PlayGroundContainer } from "./styles";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
+import { checkCard } from "../../redux/cards/cardSlice";
+import Card from "../Card/Card";
+import {
+  GameBoard,
+  PlayGroundContainer,
+  WarningText,
+  WarningWrapper,
+} from "./styles";
 
 const PlayGround: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const cards = useAppSelector((state) => state.card.items);
+  const openedCards = useAppSelector((state) => state.card.openedCards);
+
+  useEffect(() => {
+    if (openedCards.length === 2) {
+      setTimeout(() => {
+        dispatch(checkCard());
+      }, 500);
+    }
+  }, [dispatch, openedCards]);
+
   return (
     <PlayGroundContainer>
-      <GameBoard />
+      <GameBoard>
+        {cards.length > 0 ? (
+          cards.map((card) => <Card key={card.id} card={card} />)
+        ) : (
+          <WarningWrapper>
+            <WarningText>Click start button!</WarningText>
+          </WarningWrapper>
+        )}
+      </GameBoard>
     </PlayGroundContainer>
   );
 };
--- src/components/PlayGround/styles.ts
+++ src/components/PlayGround/styles.ts
@@ -11,15 +11,16 @@ export const PlayGroundContainer = styled.div`
 `;
 
 export const GameBoard = styled.div`
-  width: 60%;
-  height: 90%;
-  margin-top: -180px;
+  width: 80%;
+  height: 100%;
+  margin-top: -75px;
   background-color: var(--light-grey-color);
   border: 5px solid var(--orange-color);
   border-radius: 20px;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
+  padding: 15px 15px;
 
   @media (max-width: 1200px) {
     width: 70%;
@@ -30,10 +31,24 @@ export const GameBoard = styled.div`
   }
 
   @media (max-width: 680px) {
-    width: 90%;
+    width: 95%;
   }
 
   @media (max-width: 370px) {
-    margin-top: -120px;
+    margin-top: -70px;
   }
+`;
+
+export const WarningWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const WarningText = styled.p`
+  color: var(--purple-color);
+  font-size: 30px;
+  font-weight: bold;
 `;
